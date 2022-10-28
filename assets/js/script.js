676bd92d122463da9b47c6cf17bacc5c9830a4a5
Leonardo Sampaio <profleonardo95@gmail.com>
2022-10-28 13:37:10 -0300

    chat e envio de msg quase lá
    falta o utimo requisito de cada coisa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,103 @@
 let mensagens =[];
+let usuario = '';
+
+pegarMensagens();
+entrarSala();
+setInterval(pegarMensagens, 3000);
+setInterval(manterConexao, 5000);
+
 
 function pegarMensagens(){
     const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-    promessa.then(respostaChegou);
-    promessa.catch(erro);
+    promessa.then(respostaMsgChegou);
+    promessa.catch(respostaMsgErro);
+
+    
 }
 
-pegarMensagens();
-
-function respostaChegou(resposta){
-    console.log('Resposta chegou!!!')
+function respostaMsgChegou(resposta){
+    console.log('Resposta das mensagens chegou!!!')
     mensagens = resposta.data
-    console.log(mensagens)
     renderizarMensagem();
 }
 
-function erro(){
-    console.log('Resposta deu ERRO!!!')
+function respostaMsgErro(){
+    console.log('Resposta das mensagens deu ERRO!!!')
 }
 
-function addMensagens(){
+function manterConexao(){
+    const nome = {
+        name: "Leonardo Sampaio"
+    }
 
+    const promesse = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", nome);
+    promesse.then(manterNomeCerto);
+    promesse.catch(manterNomeErrado);
+}
+
+function manterNomeCerto(resposta) {
+    console.log(`${usuario} continua na sala.`)
+    pegarMensagens()
+}
+
+function manterNomeErrado(erro) {
+    console.log(`${usuario} saiu da sala.`)
+}
+
+function entrarSala(){
+    const nome = {
+        name: "Leonardo Sampaio"
+    }
+    usuario = nome.name;
+
+    const promesse = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", nome);
+    promesse.then(entradaNomeCerto);
+    promesse.catch(entradaNomeErrado);
+}
+
+function entradaNomeCerto(resposta) {
+    console.log(`${usuario} entou!!`)
+    pegarMensagens()
+}
+
+function entradaNomeErrado(erro) {
+    console.log(erro);
+    
+    if ( erro.response.status === 400) {
+        alert("Nome ja exixte");
+    }else{
+        alert("ocorreu um erro!");
+    }
+}
+
+function enviarMensagens(){
+    let msg = document.querySelector('.enviar-msg').value;
+
+    let novaMsg = 
+        {
+            from: usuario,
+            to: "Todos",
+            text: msg,
+            type: "message"
+        }
+
+
+    let promese = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",novaMsg);
+    promese.then(enviarMsgDeuCerto);
+    promese.catch(enviarMsgDeuErrado);
+
+    renderizarMensagem();
+}
+
+function enviarMsgDeuCerto(resposta) {
+    console.log(resposta.status)
+    console.log("Sua mensagem foi adicionada!")
+    pegarMensagens();
+}
+
+function enviarMsgDeuErrado(erro) {
+    alert("Desconectado!")
+    window.location.reload()
 }
 
 function renderizarMensagem(){
@@ -38,6 +115,11 @@ function renderizarMensagem(){
             </div>
         `;
     }
+    rolarUltima();
+}
 
-    console.log(listaMensagens);
+function rolarUltima(){
+    let elementoVisivel = document.querySelectorAll('.msg');
+    elementoVisivel = elementoVisivel[elementoVisivel.length-1];
+    elementoVisivel.scrollIntoView();
 }
